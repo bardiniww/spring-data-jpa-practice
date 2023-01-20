9bc31ac1020a18cb90ac517b46d4f254f05f32cf
bardiniww <bardiniww@gmail.com>
2023-01-21 01:30:24 +0800

    named params in repo queries

--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,11 +16,15 @@ interface StudentRepository extends JpaRepository<Student, Long> {
 
     @Query(
             "SELECT s FROM Student s " +
-                    "WHERE s.firstName = ?1 " +
-                    "AND s.age >= ?2"
+                    "WHERE s.firstName = :firstName " +
+                    "AND s.age >= :age"
     )
-    List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual(String firstName, Integer age);
+    List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual(
+            @Param("firstName") String firstName,
+            @Param("age") Integer age
+    );
 
+    //not recommended to use native, because of in case if database provider changes, a query will not work
     @Query(
             value = "SELECT * FROM student s " +
                     "WHERE s.firstName = ?1 " +
